2ce40bd68a283e6e7acc11db67f3a1079d0f417d
codebayby <2467815216@qq.com>
2018-01-20 19:21:45 +0800

    更新脚本

--- DownPhUrl.py
+++ DownPhUrl.py
@@ -18,19 +18,25 @@ ssl._create_default_https_context = ssl._create_unverified_context
 UA_LIST = [ "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24" ]
 header={ 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6', 'Connection': 'keep-alive','User-Agent': random.choice(UA_LIST) }
 clients=pymongo.MongoClient('106.15.224.237')
-date=time.strftime("%F", time.localtime())
-dbname="prohub"+date
+date=time.strftime("%F-%T", time.localtime())
+dbname="down_url"
 db=clients[dbname]
-col1=db['title']
-col2=db['ph_url']
+col1=db['detail']
+
 
 #待完善功能
 #1.更换header
 #2.存取mongo数据
 
 
-def Get_url_mongo(title,ph_url):
+def Get_url_mongo(ph_url):
+
 	pass
+
+
+def save_downurl_mongo(title,downurl):
+    detail1 = {'标题': title, '下载URL':downurl,'时间':date}
+    col1.insert(detail1)
 
 def callbackfunc(blocknum, blocksize, totalsize):
     '''回调函数
--- GetPhUrl.py
+++ GetPhUrl.py
@@ -13,19 +13,19 @@ ssl._create_default_https_context = ssl._create_unverified_context
 UA_LIST = [ "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24" ]
 header={ 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Encoding': 'gzip, deflate, sdch', 'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6', 'Connection': 'keep-alive','User-Agent': random.choice(UA_LIST) }
 clients=pymongo.MongoClient('106.15.224.237')
-date=time.strftime("%F", time.localtime())
-dbname="prohub"+date
+date=time.strftime("%F-%T", time.localtime())
+dbname="pornhub"+date
 db=clients[dbname]
-col1=db['title']
-col2=db['ph_url']
+col1=db['ph_url']
 
 #待完善功能
 #1.更换header
 #2.存取mongo数据
 
 
-def url_save_mongo(title,ph_url):
-	pass
+def url_save_mongo(ph_url):
+    detail1={'ph_url':ph_url,'时间':date}
+    col1.insert(detail1)
 
 
 def get_ph_url(response):
@@ -34,8 +34,9 @@ def get_ph_url(response):
         selector = Selector(text=response)
         divs = selector.xpath('//div[re:test(@class,"thumbnail-info-wrapper")]//@href').extract()
         for div in divs:
-           viewurl = url + div
-           print("viewurl:" + viewurl)
+            viewurl = url + div
+           # print("viewurl:" + viewurl)
+            url_save_mongo(viewurl)
     except:
         pass
 
