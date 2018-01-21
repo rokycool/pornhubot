52326881660a14871315931ca0dc91e8e081b9e1
codebayby <2467815216@qq.com>
2018-01-21 13:10:28 +0800

    更新脚本

--- GetPhUrl.py
+++ GetPhUrl.py
@@ -24,13 +24,10 @@ col1=db['ph_url']
 
 
 def url_save_mongo(ph_url):
-    detail1={}
+    detail1={'ph_url':ph_url}
     print("ph_url:"+ph_url)
-    detail1={'ph_url':'https://jp.pornhub.com/view_video.php?viewkey=ph59c67c843d568','时间':'date'}
     print("正在插入"+detail1)
     col1.insert(detail1)
-
-
 def get_ph_url(response):
     #这一段经常出问题 尝试3次
     try:
