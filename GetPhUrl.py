6856748280d2585209c0fb438a55bf9e6f06c945
codebayby <2467815216@qq.com>
2018-01-21 13:13:36 +0800

    更新脚本

--- GetPhUrl.py
+++ GetPhUrl.py
@@ -24,7 +24,7 @@ col1=db['ph_url']
 
 
 def url_save_mongo(ph_url):
-    detail1={'ph_url':str(ph_url)}
+    detail1={'ph_url':ph_url}
     print("ph_url:"+ph_url)
     print("正在插入"+detail1)
     col1.insert(detail1)
@@ -35,7 +35,7 @@ def get_ph_url(response):
         divs = selector.xpath('//div[re:test(@class,"thumbnail-info-wrapper")]//@href').extract()
         for div in divs:
             ph_url = url + div
-            # print("viewurl:" + ph_url)
+            ph_url = str(ph_url)
             try:
                 url_save_mongo(ph_url)
             except Exception as e:
